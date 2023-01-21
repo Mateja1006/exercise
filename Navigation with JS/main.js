e36f19ab0d1fbbe119c1d60ef9613c80d1e217d5
Mateja <117488374+Mateja1006@users.noreply.github.com>
2023-01-21 19:45:21 +0100

    Add files via upload
    navigation with some js elements. it is not responsive at all that is yet to do.

---
+++ Navigation with JS/main.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu () {
+    subMenu.classList.toggle("open-menu")
+}
